912279dca7d22b1ce697022f855fef801946a0f8
Gilang Pratama <gilpratama@outlook.com>
2023-09-02 13:41:47 +0700

    feat: :art: added login page

---
+++ tech-blog/src/LoginForm.jsx
@@ -0,0 +1,55 @@
+// LoginForm.js
+import React, { useState } from 'react';
+
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // You can add your login logic here.
+    // Typically, you'd make an API call to authenticate the user.
+
+    // For demonstration purposes, we'll just display the email and password for now.
+    alert(`Email: ${email}, Password: ${password}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+      </div>
+      <div>
+        <button type="submit">Login</button>
+      </div>
+    </form>
+  );
+}
+
+export default LoginForm;
